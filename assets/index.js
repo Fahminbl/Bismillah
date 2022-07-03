000ad6e54e0eb6a862c7d2d68a45e7708f58dafe
Fahminbl <79783544+Fahminbl@users.noreply.github.com>
2022-07-03 14:19:41 +0700

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,25 @@
-function duar() {
-    var x = document.getElementById("navAtasku");
-    if (x.className === "navAtas") {
-        x.className += " responsive";
-    } else {x.className = "navAtas";
-}
-}
+//function duar() {
+//    var x = document.getElementById("navAtasku");
+//    if (x.className === "navAtas") {
+  //      x.className += " responsive";
+    //} else {x.className = "navAtas";
+//}
+
+
+function dropDown() {
+    document.getElementById("Drop").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.active')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
