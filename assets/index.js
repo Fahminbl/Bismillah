4bf9e6acef900dbb2dc3a487062b139ab30beb1a
Fahminbl <79783544+Fahminbl@users.noreply.github.com>
2022-07-04 22:57:32 +0700

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -23,3 +23,29 @@ function dropDown() {
       }
     }
   }
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slider");
+  let dots = document.getElementsByClassName("titik");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" aktif", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " aktif";
+}
